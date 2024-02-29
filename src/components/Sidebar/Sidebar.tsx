a76f99f48919cf817f9d9a300d57d950a22a8d68
huyngse <ngogiahuy775309@gmail.com>
2024-02-29 08:38:53 +0700

    Add tooltip to sidebar items

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -10,114 +10,168 @@ import { GrTechnology } from "react-icons/gr";
 import { BsPersonVideo } from "react-icons/bs";
 import { PiTelevisionBold } from "react-icons/pi";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import SidebarItem from "./SidebarItem";
 
 type SidebarProps = {
   isCollapse?: boolean;
 };
 
 const Sidebar = ({ isCollapse = false }: SidebarProps) => {
-  const sidebarButtonStyle =
-  `w-full gap-2 flex  bg-white text-black hover:bg-slate-200 ${isCollapse ? "justify-center" : "justify-start"}`;
+  const sidebarButtonStyle = `w-full gap-2 flex  bg-white text-black hover:bg-slate-200 ${
+    isCollapse ? "justify-center" : "justify-start"
+  }`;
   return (
     <div className={`${!isCollapse ? "max-w-[240px]" : "max-w-fit"}`}>
-      <ul className="flex flex-col">
+      <ul className={`flex flex-col ${isCollapse && "items-center"}`}>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Home"
+          >
             <IoHomeOutline />
             {!isCollapse && <p>Home</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Gaming"
+          >
             <IoGameControllerOutline />
             {!isCollapse && <p>Gaming</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Sports"
+          >
             <GoTrophy />
             {!isCollapse && <p>Sports</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Entertainment"
+          >
             <PiTelevisionBold />
             {!isCollapse && <p>Entertainment</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Technology"
+          >
             <GrTechnology />
             {!isCollapse && <p>Technology</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Music"
+          >
             <IoMusicalNotesOutline />
             {!isCollapse && <p>Music</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Blog"
+          >
             <BsPersonVideo />
             {!isCollapse && <p>Blog</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="News"
+          >
             <LuNewspaper />
             {!isCollapse && <p>News</p>}
-          </Button>
+          </SidebarItem>
         </li>
       </ul>
       <hr className="m-3" />
       {!isCollapse && <h3 className="m-3">Subscribed</h3>}
 
-      <ul>
+      <ul className={`flex flex-col ${isCollapse && "items-center"}`}>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Shiey"
+          >
             <Avatar className="w-[24px] h-[24px]">
               <AvatarImage src="https://yt3.ggpht.com/t1m5ErpCFmyYSQG5QmECS3Yh_T64hfUlsvHpK3Ck_XYv5fCpz6p1w3h4Zcj0P4GzoqQYsGkG=s88-c-k-c0x00ffffff-no-rj" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             {!isCollapse && <p>Shiey</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Adventuring with Nala"
+          >
             <Avatar className="w-[24px] h-[24px]">
               <AvatarImage src="https://yt3.ggpht.com/3CRWKfZlf5EnmlvvV_XmMyL9uXYFoIB79h0a9Ucpnpt7P4_yv5SqGdJriykHeXTpgDpChBdu=s88-c-k-c0x00ffffff-no-rj" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             {!isCollapse && <p>Adventuring with Nala</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Ze Frank"
+          >
             <Avatar className="w-[24px] h-[24px]">
               <AvatarImage src="https://yt3.ggpht.com/ytc/AIdro_mk_RWXQdJSdSqrM41HBpozpxT3bQkW83csX__g=s88-c-k-c0x00ffffff-no-rj" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             {!isCollapse && <p>Ze Frank</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Shimanagi"
+          >
             <Avatar className="w-[24px] h-[24px]">
               <AvatarImage src="https://yt3.ggpht.com/_P2SZSNumrneZ-5la5f3zW_h0U43ftBEOBFl2bhM8G7-0GGDxZNSw0Sep7_NfR2NMayxelLAyE0=s88-c-k-c0x00ffffff-no-rj" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             {!isCollapse && <p>Shimanagi</p>}
-          </Button>
+          </SidebarItem>
         </li>
         <li>
-          <Button className={sidebarButtonStyle}>
+          <SidebarItem
+            href="https://youtu.be/tCO4i2t-Aso?si=lIcOjwucw4UOFWw1"
+            isCollapse={isCollapse}
+            title="Let's Game It Out"
+          >
             <Avatar className="w-[24px] h-[24px]">
               <AvatarImage src="https://yt3.ggpht.com/ytc/AIdro_ngIjtanAPdrwevZ3hqb2ZBYULw5hXCLP_-fO5bRQ=s88-c-k-c0x00ffffff-no-rj" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             {!isCollapse && <p>Let's Game It Out</p>}
-          </Button>
+          </SidebarItem>
         </li>
       </ul>
     </div>
--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -1,13 +1,37 @@
 import { ReactNode } from "react";
 import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
 type SidebarItemProps = {
   href: string;
-  children: ReactNode;
+  children: ReactNode | string;
   title: string;
+  isCollapse?: boolean;
 };
-const SidebarItem = ({ children }: SidebarItemProps) => {
-  return <Button>{children}</Button>;
+const SidebarItem = ({
+  children,
+  href,
+  isCollapse = false,
+  title,
+}: SidebarItemProps) => {
+  const sidebarButtonStyle = `w-full gap-2 flex  bg-white text-black hover:bg-slate-200 ${
+    isCollapse ? "justify-center" : "justify-start"
+  }`;
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger className="w-full">
+          <Link to={href} >
+            <Button className={sidebarButtonStyle}>{children}</Button>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{title}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 };
 
 export default SidebarItem;
